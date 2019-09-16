971926afa4b882b47b393c1b787f3641b1727da5
shadow <shadow@LAPTOP-AFD369ML>
2019-09-16 15:49:29 +0800

    添加项目文件。

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace practice1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("输出1-9的数字");
+
+        }
+    }
+}
